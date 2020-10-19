417442d35ff625344df4a4ef8ab1288a0c11695d
Esther Goldberg <30660571+Esther-Goldberg@users.noreply.github.com>
2020-10-19 02:33:23 -0400

    added tags

--- app/models.py
+++ app/models.py
@@ -15,8 +15,11 @@ def load_user(id):
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-                     )
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
+tags = db.Table('tags',
+                db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')))
 
 
 class User(UserMixin, db.Model):
@@ -90,5 +93,18 @@ class Post(db.Model):
 
     parent = db.relationship('Post', remote_side=[id], backref='children')
 
+    tags = db.relationship('Tag', secondary=tags, backref=db.backref('post', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+    def add_tag(self, tag):
+        self.tags.append(tag)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+
+    def __repr__(self):
+        return '#{}'.format(self.name)
